228405a106f8db3ea7b76a777d3372c8748795ef
koyomiararagi <franko753_95@hotmail.com>
2017-04-04 23:21:38 -0500

    use-String

---
+++ src/Cadenas.java
@@ -0,0 +1,11 @@
+
+public class Cadenas {
+
+	public static void main(String[] args) {
+		String nombre="koyomi";
+		System.out.println(nombre+" tamaño : "+nombre.length()+ "posicion 2 : "+nombre.charAt(2));
+		
+
+	}
+
+}
