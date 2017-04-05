dae3cbd93d02efd34cd20710dfef3a24fb877973
koyomiararagi <franko753_95@hotmail.com>
2017-04-04 23:33:37 -0500

    use-substring

--- src/Cadenas.java
+++ src/Cadenas.java
@@ -2,9 +2,12 @@
 public class Cadenas {
 
 	public static void main(String[] args) {
-		String nombre="koyomi";
-		System.out.println(nombre+" tamaño : "+nombre.length()+ "posicion 2 : "+nombre.charAt(2));
+		String frase="Avast antivirus. Solo con sus números te puedes hacer una idea de lo bueno que es este antivirus. Más de 150 millones de usuarios en todo el mundo (el antivirus gratuito más utilizado).";
+		String frase_res=frase.substring(0,frase.length());
+		System.out.println(frase_res);
 		
+		System.out.println(frase.equals(frase_res));
+		System.out.println(frase.equalsIgnoreCase(frase_res));
 
 	}
 
