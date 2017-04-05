e8d2e48a0660cee7002e70e7898631773951e4f6
koyomiararagi <franko753_95@hotmail.com>
2017-04-04 22:45:45 -0500

    variavles

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte edad=35;//menor
+		short edad1=40;
+		int edad2=50;//mayor
+		System.out.println("Edad es : "+edad);
+		System.out.println("Edad es : "+edad1);
+		System.out.println("Edad es : "+edad2);
+	}
+
+}
